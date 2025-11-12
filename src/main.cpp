e8a57ee055a9100a20dae158147af938bb712e0f
AlkkuS <palvelut@asarja.fi>
2025-11-12 10:31:16 +0000

    Clean up main

--- src/main.cpp
+++ src/main.cpp
@@ -28,27 +28,6 @@ int main() {
     core::TestGameInitializationAndTurns();
     core::TestGameSaveAndLoad();
 
-    // Graphics init
-    ////sf::RenderWindow window(sf::VideoMode(constants::kInitWindowWidth, constants::kInitWindowHeight), "StrategyGame");
-    ////sf::View view = window.getDefaultView();
-    ////sf::Vector2f view_size = view.getSize();
-
-    // Game state
-    bool start = false;
-
-    // Initialize font with shared_ptr
-    ////auto font = std::make_shared<sf::Font>();
-    ////if (!font->loadFromFile("./texture/times.ttf")){
-    ////    return EXIT_FAILURE;
-    ////}
-    
-    // Main menu init
-    ////ui::MainMenu main_menu;
-    ////main_menu.Initialize(font, view_size);
-
-    // Map renderer init
-    ////ui::MapRenderer map_renderer;
-
     // init game
     std::vector<core::Game::PlayerInit> players;
     core::Game game;
@@ -60,88 +39,58 @@ int main() {
     while (user_interface.GetWindow().isOpen()) {
         
         user_interface.UpdateMousePos();
-        ////sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
 
         // Handle events
-        ////sf::Event event;
         while (user_interface.PollEvent()) {
             
             user_interface.HandleGeneralEvents();
-            ////if (event.type == sf::Event::Closed)
-            ////    window.close();
-            ////
-            ////if (event.type == sf::Event::Resized) {
-            ////    view.setSize(sf::Vector2f(event.size.width, event.size.height));
-            ////    window.setView(view);
-            ////}
-            if (!start) { // main menu
+
+            // show main menu if game is not initialized yet
+            if (!game.IsInitialized()) {
                 user_interface.HandleMainMenuEvents();
-                ////if (event.type == sf::Event::MouseButtonReleased && 
-                ////    event.mouseButton.button == sf::Mouse::Left) {
 
-                //TODO: add a check for clicking stuff???
+                // Initialize game on pressing "Play"
+                if (user_interface.IsPlayClicked()) {
 
-                    // update elements that do something when LMB is released
-                    ////main_menu.UpdateLMBReleased(window); 
+                    // Get game initialization options from selectors
+                    std::vector<int> options = user_interface.GetSelectedOptions();
+                    unsigned int player_count = options[0];
+                    unsigned int map_size = options[1];
 
-                    // Initialize game on pressing "Play"
-                    if (user_interface.IsPlayClicked()) {
-
-                        // Things that are done when play is clicked
-                        start = true;
-
-                        // Get game initialization options from selectors
-                        std::vector<int> options = user_interface.GetSelectedOptions();
-                        unsigned int player_count = options[0];
-                        unsigned int map_size = options[1];
-
-                        // Create players
-                        for (unsigned int i = 0; i < player_count; ++i) {
-                            players.emplace_back(core::Game::PlayerInit{
-                                "Player " + std::to_string(i + 1),
-                                cards::Deck() // TODO: add custom starter decks
-                            });
-                        }
-
-                        // Initializing through main menu testing
-                        game.Initialize(players, map_size);
-                        user_interface.InitializeMapRenderer(game.GetMap());
-
-                        assert(game.IsInitialized() && !game.IsOver());
-                        assert(game.GetCurrentPlayer().GetName() == "Player 1");
-                        for (int i = 0; i < 5; i++) {
-                            game.NextTurn();
-                            assert(game.GetCurrentTurn() == i + 1);
-                            // Add resources to the current player for testing
-                            game.GetCurrentPlayer().AddResources({core::Resource(core::ResourceType::kGold, 10 + 2 * i)});
-                        }
-                        // Save game state after initialization
-                        std::ofstream outFile("saveFile.txt");
-                        if (outFile.is_open()) {
-                            game.Save(outFile);
-                            outFile.close();
-                        }
+                    // Create players
+                    for (unsigned int i = 0; i < player_count; ++i) {
+                        players.emplace_back(core::Game::PlayerInit{
+                            "Player " + std::to_string(i + 1),
+                            cards::Deck() // TODO: add custom starter decks
+                        });
                     }
-                //} // end if lmb is clicked
 
+                    // Initializing through main menu testing
+                    game.Initialize(players, map_size);
+                    user_interface.InitializeMapRenderer(game.GetMap());
 
+                    assert(game.IsInitialized() && !game.IsOver());
+                    assert(game.GetCurrentPlayer().GetName() == "Player 1");
+                    for (int i = 0; i < 5; i++) {
+                        game.NextTurn();
+                        assert(game.GetCurrentTurn() == i + 1);
+                        // Add resources to the current player for testing
+                        game.GetCurrentPlayer().AddResources({core::Resource(core::ResourceType::kGold, 10 + 2 * i)});
+                    }
+                    // Save game state after initialization
+                    std::ofstream outFile("saveFile.txt");
+                    if (outFile.is_open()) {
+                        game.Save(outFile);
+                        outFile.close();
+                    }
+                }
 
-                // Update the elements that do something when hovered over
-                ////main_menu.UpdateHovered(window, mousePos);
-                // Clear the screen
-                ////window.clear();
-                // Draw the sprites and selectors
-                ////main_menu.DrawTo(window);
-            } else if (start) {
-                // Map rendering loop
-                ////window.clear();
-                ////map_renderer.DrawTo(window);
+            // Render map and other game ui elements
+            } else {
+                // TODO: temporarily keep this to accomodate other ui branches
             }
         }
-        // Update the window
-        ////window.display();
-        // Update the window
-        user_interface.DrawAndDisplay(start);
+        user_interface.DrawAndDisplay(game.IsInitialized());
     }
     return 0;
 }
