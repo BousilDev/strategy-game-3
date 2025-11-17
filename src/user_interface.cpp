a3b615edb71e0fc2b9f97e4c3b203d5eb80ace5d
AlkkuS <palvelut@asarja.fi>
2025-11-17 20:35:10 +0000

    Fix error in user_interface.HandleEvent

--- src/user_interface.cpp
+++ src/user_interface.cpp
@@ -52,7 +52,7 @@ void ui::UserInterface::HandleEvent(bool start) {
     if (!start) {
         main_menu_.Update(window_, mouse_pos_, event_);
     } else {
-        info_layer_renderer_->Update(window_, mouse_pos_, event_);
+        info_layer_renderer_.Update(window_, mouse_pos_, event_);
         if (event_.type == sf::Event::MouseButtonReleased && 
             event_.mouseButton.button == sf::Mouse::Left) {
         // update map elements that do something when LMB is released
