ccf3f0cf8d78d449a4638d6f4bb18d40c334e3cc
Konsta Suuriniemi <konsta.suuriniemi@aalto.fi>
2025-11-19 17:53:36 +0200

    Fix card Play methods and add TODO to tile placement methods, which should be easy after merging into master.

--- src/card.cpp
+++ src/card.cpp
@@ -2,13 +2,12 @@
 
 namespace cards { 
 
-//TODO: Fix these to be compatible with Tile placement implementation and connect everyting.
-// bool BuildingCard::Play(world::Tile& target) {
-//     return !(target.get_terrain()->get_name() == "water" || !target.place_building(*building_));
-// }
+bool BuildingCard::Play(world::Tile& target) {
+    return !(target.get_terrain()->get_name() == "water" || !target.place_building(building_->Clone()));
+}
 
-// bool UnitCard::Play(world::Tile& target) {
-//     return !(target.get_terrain()->get_name() == "water" || !target.place_unit(*unit_));
-// }
+bool UnitCard::Play(world::Tile& target) {
+    return !(target.get_terrain()->get_name() == "water" || !target.place_unit(unit_->Clone()));
+}
 
 } // namespace cards
